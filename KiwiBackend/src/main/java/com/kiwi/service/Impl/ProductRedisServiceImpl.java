f836e12bdce571f1ada1b65735f2569672ed4ae4
HHfguzje <hoang03.it@gmail.com>
2024-04-17 11:12:26 +0700

    update log redis

--- KiwiBackend/src/main/java/com/kiwi/service/Impl/ProductRedisServiceImpl.java
+++ KiwiBackend/src/main/java/com/kiwi/service/Impl/ProductRedisServiceImpl.java
@@ -7,6 +7,8 @@ import com.kiwi.service.ProductRedisService;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,8 @@ public class ProductRedisServiceImpl implements ProductRedisService {
     private static final String PRODUCT_CACHE_KEY = "ProductCache";
     private final RedisTemplate<String, Object> redisTemplate;
     private final ObjectMapper objectMapper;
+    private static final Logger logger = LoggerFactory.getLogger(ProductRedisService.class);
+
 
     @Autowired
     public ProductRedisServiceImpl(RedisTemplate<String, Object> redisTemplate, ObjectMapper objectMapper) {
@@ -29,27 +33,30 @@ public class ProductRedisServiceImpl implements ProductRedisService {
 
     @Override
     public void clear() {
-        redisTemplate.delete(PRODUCT_CACHE_KEY);
+       boolean isDeleted= redisTemplate.delete(PRODUCT_CACHE_KEY);
+        logger.info("Đã xóa cache cho tất cả sản phẩm: {}", isDeleted ? "thành công" : "thất bại");
     }
 
     @Override
     public List<Product> getAllProducts(String keyword, Long categoryId) throws JsonProcessingException {
         String hashKey = getHashKey(keyword, categoryId);
-        List<Product> productResponses = null;
         if(redisTemplate.opsForHash().hasKey(PRODUCT_CACHE_KEY, hashKey)) {
+            logger.info("Cache truy xuất thành công cho từ khóa '{}' và danh mục ID '{}'", keyword, categoryId);
             String jsonProducts = (String) redisTemplate.opsForHash().get(PRODUCT_CACHE_KEY, hashKey);
-            productResponses = objectMapper.readValue(jsonProducts, new TypeReference<List<Product>>(){});
+            return objectMapper.readValue(jsonProducts, new TypeReference<List<Product>>(){});
+        } else {
+            logger.info("Cache không tìm thấy cho từ khóa '{}' và danh mục ID '{}'", keyword, categoryId);
         }
-        return productResponses;
+        return null;
     }
 
     @Override
-    public void saveAllProducts(List<Product> productResponses, String keyword, Long categoryId) throws JsonProcessingException {
+    public void saveAllProducts(List<Product> products, String keyword, Long categoryId) throws JsonProcessingException {
         String hashKey = getHashKey(keyword, categoryId);
-        String jsonProducts = objectMapper.writeValueAsString(productResponses);
+        String jsonProducts = objectMapper.writeValueAsString(products);
         redisTemplate.opsForHash().put(PRODUCT_CACHE_KEY, hashKey, jsonProducts);
-
         redisTemplate.expire(PRODUCT_CACHE_KEY, Duration.ofHours(1));
+        logger.info("Đã lưu vào cache danh sách sản phẩm với từ khóa '{}' và danh mục ID '{}'", keyword, categoryId);
     }
 
     private String getHashKey(String keyword, Long categoryId) {
